00c78f31b8108a18112918e9dec1d38e62bd0dc9
DanVolov <murzikpankov@gmail.com>
2026-01-31 20:46:53 +0300

    2

--- огэ/07 12 25/1.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-sum = 0
-for i in range(n):
-    x = int(input())
-    if x % 5 == 3:
-        sum += x
-print(sum)
--- огэ/07 12 25/2.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-sum = 0
-for i in range(n):
-    x = int(input())
-    if x % 9 == 5:
-        sum += x
-print(sum)
